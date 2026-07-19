d370fb973552ef6dac23893509f63ce35e2ae5cf
mide-s <mide@monadhackathon.io>
2026-07-19 16:54:15 -0700

    Fix unused sendTxReceipt variable in Quest2.tsx

--- frontend/src/quests/Quest2.tsx
+++ frontend/src/quests/Quest2.tsx
@@ -22,8 +22,8 @@ export function Quest2({ onComplete, addLog, openExplainer }: Quest2Props) {
   const [sendTxHash, setSendTxHash] = useState<`0x${string}` | undefined>(undefined);
   const [mintTxHash, setMintTxHash] = useState<`0x${string}` | undefined>(undefined);
 
-  const { isLoading: isSendWaiting, isSuccess: isSendSuccess, data: sendTxReceipt } = useWaitForTransactionReceipt({ hash: sendTxHash });
-  const { isLoading: isMintWaiting, isSuccess: isMintSuccess } = useWaitForTransactionReceipt({ hash: mintTxHash });
+  const { isSuccess: isSendSuccess } = useWaitForTransactionReceipt({ hash: sendTxHash });
+  const { isSuccess: isMintSuccess } = useWaitForTransactionReceipt({ hash: mintTxHash });
 
   const handleSendTransaction = () => {
     openExplainer({
